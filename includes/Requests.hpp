d422301a8210bbfaba56e5498f68568fed9d3b8b
Nicosator94 <nicosator94@gmail.com>
2024-07-17 17:35:51 +0200

    get method

--- includes/Requests.hpp
+++ includes/Requests.hpp
@@ -3,34 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   Requests.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 11:57:21 by niromano          #+#    #+#             */
-/*   Updated: 2024/07/16 14:25:22 by elrichar         ###   ########.fr       */
+/*   Updated: 2024/07/17 12:25:54 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <string>
+#include <vector>
 
 class Requests {
 
 	public :
 
-		Requests(const std::string &type, const std::string &path, const std::string &method);
+		Requests(const std::string &method, const std::string &path, const std::string &protocol, std::vector<std::string> &accept);
 		~Requests();
-		std::string getType();
-		std::string getPath();
-		std::string getMethod();
 		std::string getResponse();
-		bool isSyntaxError();
 
 	private :
 
-		const std::string _type;
-		const std::string _path;
 		const std::string _method;
+		const std::string _path;
+		const std::string _protocol;
+		std::vector<std::string> _accept;
 };
 
 Requests readRequest(std::string buf);
--- srcs/Requests.cpp
+++ srcs/Requests.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Requests.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elrichar <elrichar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: niromano <niromano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 11:56:05 by niromano          #+#    #+#             */
-/*   Updated: 2024/07/16 14:56:30 by elrichar         ###   ########.fr       */
+/*   Updated: 2024/07/17 17:26:07 by niromano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,57 @@
 #include <fstream>
 #include "../includes/Requests.hpp"
 
+std::vector<std::string> split(std::string buf, std::string find) {
+	std::vector<std::string> vector;
+	for (int i = 0; !buf.empty(); i++) {
+		vector.push_back(buf.substr(0, buf.find(find)));
+		buf.erase(0, vector[i].size() + 1);
+	}
+	return vector;
+}
+
+bool isMethodValid(std::string line) {
+	std::vector<std::string> method;
+	method.push_back("GET");
+	method.push_back("POST");
+	method.push_back("DELETE");
+	for (unsigned int i = 0; i < method.size(); i++)
+		if (!line.compare(0, method[i].size(), method[i]))
+			return true;
+	return false;
+}
+
+std::vector<std::string> readMethod(std::string line) {
+	std::vector<std::string> method = split(line, " ");
+	for (unsigned int i = 0; i < method.size(); i++)
+		std::cout << method[i] << std::endl;
+	return method;
+}
+
+void readTest(std::string buf) {
+	std::cout << buf << std::endl;
+	std::vector<std::string> request = split(buf, "\n");
+	for (unsigned int i = 0; i < request.size(); i++)
+		if (isMethodValid(request[i]))
+			std::vector<std::string> method = readMethod(request[i]);		
+}
+
 Requests readRequest(std::string buf) {
+	readTest(buf);
 	std::string firstLine = buf.substr(0, buf.find("\n"));
-	const std::string type = firstLine.substr(0, firstLine.find(" "));
-	firstLine.erase(0, type.size() + 1);
+	const std::string method = firstLine.substr(0, firstLine.find(" "));
+	firstLine.erase(0, method.size() + 1);
 	const std::string path = "." + firstLine.substr(0, firstLine.find(" "));
 	firstLine.erase(0, path.size() + 1);
-	const std::string method = firstLine;
-	Requests req(type, path, method);
+	const std::string protocol = firstLine;
+	std::vector<std::string> accept;
+	Requests req(method, path, protocol, accept);
 	return req;
 }
 
-Requests::Requests(const std::string &type, const std::string &path, const std::string &method) : _type(type), _path(path), _method(method) {}
+Requests::Requests(const std::string &method, const std::string &path, const std::string &protocol, std::vector<std::string> &accept) : 
+	_method(method), _path(path), _protocol(protocol), _accept(accept) {}
 Requests::~Requests() {}
-std::string Requests::getType() {return this->_type;}
-std::string Requests::getPath() {return this->_path;}
-std::string Requests::getMethod() {return this->_method;}
 
 std::string getPage(std::string filename, std::string responseStatus) {
     std::ifstream fd(filename.c_str());
@@ -44,16 +79,11 @@ std::string getPage(std::string filename, std::string responseStatus) {
 	return response;
 }
 
-bool Requests::isSyntaxError() {
-	if (0)
-		return true;
-	return false;
-}
-
 std::string Requests::getResponse() {
 	chdir("./pages");
-	if (isSyntaxError())
-		return getPage("error/400.html", "HTTP/1.1 400 Bad Request\n\n");
+	// for (unsigned int i = 0; i < this->_accept.size(); i++) {
+	// 	std::cout << this->_accept[i] << std::endl;
+	// }
 	if (access((this->_path).c_str(), F_OK))
 		return getPage("error/404.html", "HTTP/1.1 404 Not Found\n\n");
 	if (access((this->_path).c_str(), R_OK))
